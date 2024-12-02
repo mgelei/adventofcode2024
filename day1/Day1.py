e5a58017dacc5a6563b0557d79af55b480ea101b
Mate Gelei-Szego <34216064+mgelei@users.noreply.github.com>
2024-12-02 10:34:59 +0100

    Day 1

---
+++ day1/Day1.py
@@ -0,0 +1,20 @@
+﻿from os.path import split
+
+if __name__ == '__main__':
+    left = []
+    right = []
+    distance = 0
+    
+    with open('input.txt') as f:
+        for line in f:
+            l, _, _, r = line.split(' ')
+            left.append(int(l))
+            right.append(int(r))
+            
+    left = sorted(left)
+    right = sorted(right)
+    
+    for i in range(0, len(left)):
+        distance += abs(left[i] - right[i])
+        
+    print(distance)
