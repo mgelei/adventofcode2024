aea8ef6a5627c33e6934ff6bbc2316cb2e0a491a
Mate Gelei-Szego <34216064+mgelei@users.noreply.github.com>
2024-12-02 10:51:22 +0100

    Day 2

---
+++ day2/Day2.py
@@ -0,0 +1,31 @@
+﻿from pprint import pprint
+
+def is_safe(report):
+    # The levels are either all increasing or all decreasing.
+    # Any two adjacent levels differ by at least one and at most three.
+    increasing = True
+    decreasing = True
+    adjacent = True
+    
+    for i in range(0, len(report) - 1):
+        left = report[i]
+        right = report[i + 1]
+        if(left > right):
+            increasing = False
+        if(left < right):
+            decreasing = False
+        if(abs(left - right) > 3 or abs(left - right) < 1):
+            adjacent = False
+            
+    print(increasing, decreasing, adjacent)
+            
+    return (increasing or decreasing) and adjacent
+
+if __name__ == '__main__':
+    reports = []
+    
+    with open('input.txt') as f:
+        reports = [[int(num) for num in line.split()] for line in f]
+        
+    safe_reports = [report for report in reports if is_safe(report)]
+    print(len(safe_reports))
