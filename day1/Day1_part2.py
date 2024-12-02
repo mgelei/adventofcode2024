dec91ff64f9390d12d28266926191e0388e080e5
Mate Gelei-Szego <34216064+mgelei@users.noreply.github.com>
2024-12-02 10:38:33 +0100

    Day 1, part 2

---
+++ day1/Day1_part2.py
@@ -0,0 +1,19 @@
+﻿if __name__ == '__main__':
+    left = []
+    right = []
+    similarity = 0
+    
+    with open('input.txt') as f:
+        for line in f:
+            l, _, _, r = line.split(' ')
+            left.append(int(l))
+            right.append(int(r))
+            
+    left = sorted(left)
+    right = sorted(right)
+    
+    for i in range(0, len(left)):
+        item = left[i]
+        similarity += (right.count(item) * item)
+        
+    print(similarity)
