6a16c418a73793ea880d274da22e0e886cd2f544
Mate Gelei-Szego <34216064+mgelei@users.noreply.github.com>
2024-12-03 10:18:07 +0100

    Day 3

---
+++ day3/Day3.py
@@ -0,0 +1,12 @@
+﻿import re
+from pprint import pprint
+
+if __name__ == '__main__':
+    pattern = re.compile(r'mul\((\d+),(\d+)\)')
+    with open('input.txt') as f:
+        raw_content = f.read()
+    valid_multiplications = re.findall(pattern, raw_content)
+    pprint(valid_multiplications)
+    
+    result = sum([int(a) * int(b) for a, b in valid_multiplications])
+    print(result)
