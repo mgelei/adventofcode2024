6199a272624500d7cbbe0b2c4a046f200a4c215d
Mate Gelei-Szego <34216064+mgelei@users.noreply.github.com>
2024-12-04 14:56:57 +0100

    Day 4, part 2

---
+++ day4/Day4_part2.py
@@ -0,0 +1,27 @@
+﻿from pprint import pprint
+
+def is_inside(i, j):
+    return 0 <= i < length and 0 <= j < length
+
+if __name__ == '__main__':
+    matrix = []
+    counter = 0
+
+    with open('input', 'r') as f:
+        for line in f:
+            matrix.append(list(line.strip()))
+    length = len(matrix)
+
+    for i in range(length):
+        for j in range(length):
+            if matrix[i][j] != 'A': # "A" would be the middle letter
+                continue
+            if not is_inside(i+1, j+1) or not is_inside(i-1, j-1) or not is_inside(i+1, j-1) or not is_inside(i-1, j+1):
+                continue
+            if not (matrix[i-1][j-1], matrix[i+1][j+1]) in (('M', 'S'), ('S', 'M')):
+                continue
+            if not (matrix[i+1][j-1], matrix[i-1][j+1]) in (('M', 'S'), ('S', 'M')):
+                continue
+            counter += 1
+    
+    print(counter)
